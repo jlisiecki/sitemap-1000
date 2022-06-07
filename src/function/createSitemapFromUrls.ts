90736ed9db3963b06bf587a6c33bc3a4ecaa25df
Jakub Lisiecki <jakub@lisiecki.xyz>
2022-06-07 10:33:28 +0200

    namespace fix in sitemap generation

--- src/function/createSitemapFromUrls.ts
+++ src/function/createSitemapFromUrls.ts
@@ -2,22 +2,24 @@ import { JSDOM } from 'jsdom';
 import { Url } from './getUrlsFromMaps';
 
 export default function createSitemapFromUrls(urls: Url[]) {
-    const { document } = new JSDOM(
-        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" />',
-        {
-            contentType: 'application/xml',
-        }
-    ).window;
+    const nameSpace = 'http://www.sitemaps.org/schemas/sitemap/0.9';
+
+    const { document } = new JSDOM(`<urlset xmlns="${nameSpace}" />`, {
+        contentType: 'application/xml',
+    }).window;
 
     const urlsetElement = document.querySelector('urlset');
 
     urls.forEach((url) => {
-        const urlElement = document.createElement('url');
-        const locElement = document.createElement('loc');
+        const urlElement = document.createElementNS(nameSpace, 'url');
+        const locElement = document.createElementNS(nameSpace, 'loc');
         locElement.textContent = url.loc.trim();
         urlElement.append(locElement);
         if (url.lastmod) {
-            const lastmodElement = document.createElement('lastmod');
+            const lastmodElement = document.createElementNS(
+                nameSpace,
+                'lastmod'
+            );
             lastmodElement.textContent = url.lastmod.trim();
             urlElement.append(lastmodElement);
         }
@@ -26,6 +28,6 @@ export default function createSitemapFromUrls(urls: Url[]) {
 
     return (
         '<?xml version="1.0" encoding="UTF-8"?>' +
-        document.documentElement.outerHTML.replaceAll(' xmlns=""', '')
+        document.documentElement.outerHTML
     );
 }
