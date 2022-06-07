134851bda4a8ef8459cfecb955937d4600505ca1
Jakub Lisiecki <jakub@lisiecki.xyz>
2022-06-07 10:59:36 +0200

    clean implemetation of sitemap creator

--- src/function/createSitemapFromUrls.ts
+++ src/function/createSitemapFromUrls.ts
@@ -2,29 +2,31 @@ import { JSDOM } from 'jsdom';
 import { Url } from './getUrlsFromMaps';
 
 export default function createSitemapFromUrls(urls: Url[]) {
-    const nameSpace = 'http://www.sitemaps.org/schemas/sitemap/0.9';
-
-    const { document } = new JSDOM(`<urlset xmlns="${nameSpace}" />`, {
+    const { document } = new JSDOM('<root />', {
         contentType: 'application/xml',
     }).window;
 
-    const urlsetElement = document.querySelector('urlset');
+    const namespaceURI = 'http://www.sitemaps.org/schemas/sitemap/0.9';
+    const urlsetElement = document.createElementNS(namespaceURI, 'urlset');
+    document.documentElement.replaceWith(urlsetElement);
 
     urls.forEach((url) => {
-        const urlElement = document.createElementNS(nameSpace, 'url');
-        const locElement = document.createElementNS(nameSpace, 'loc');
+        const urlElement = document.createElementNS(namespaceURI, 'url');
+        const locElement = document.createElementNS(namespaceURI, 'loc');
         locElement.textContent = url.loc.trim();
         urlElement.append(locElement);
         if (url.lastmod) {
             const lastmodElement = document.createElementNS(
-                nameSpace,
+                namespaceURI,
                 'lastmod'
             );
             lastmodElement.textContent = url.lastmod.trim();
             urlElement.append(lastmodElement);
         }
-        urlsetElement?.appendChild(urlElement);
+        urlsetElement.appendChild(urlElement);
     });
+
+    console.log(document.documentElement.outerHTML);
 
     return (
         '<?xml version="1.0" encoding="UTF-8"?>' +
